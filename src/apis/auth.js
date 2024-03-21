a8f36811babf8bacf7c14319762f24ed1fbff781
kumaar <kumaar@DESKTOP-UL0FUTT.localdomain>
2024-03-21 20:09:59 +0530

    first commit

---
+++ src/apis/auth.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const registerUser = async ({ email, password, mobile, name }) => {
+    try {
+        const reqUrl = `http://localhost:3000/api/v1/auth/register`;
+        const response = await axios.post(reqUrl, {
+            email,
+            password,
+            mobile,
+            name,
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+        alert("Something went wrong!");
+    }
+};
+
+export const loginUser = async ({ email, password }) => {
+    try {
+        const reqUrl = `http://localhost:3000/api/v1/auth/login`;
+        const response = await axios.post(reqUrl, {
+            email,
+            password,
+        });
+        localStorage.setItem("token", response.data.token);
+        return response.data.name;
+    } catch (error) {
+        console.log(error);
+        alert("Something went wrong!");
+    }
+};
